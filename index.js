5e3c385e2ef8dfd62353807c7064f530ee49b76e
Dmitry Sadovsky <dimailn9@gmail.com>
2023-01-19 15:41:49 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const DEFAULT_ORIGIN = 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest'
+
+export default (
+  {
+    token,
+    origin = DEFAULT_ORIGIN
+  } = {}
+) => ({
+  name: 'dadata',
+  setup({onLoad, form: {$http}}) {
+    onLoad(async ({query, entity, limit}) => {
+      const data = {
+        query,
+        count: limit
+      }
+
+      const headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+
+      if (token) {
+        headers['Authorization'] = `Token ${token}`
+      } else if (origin === DEFAULT_ORIGIN) {
+        console.error('[vrf-dadata] You need to specify token to make Dadata requests')
+      }
+
+      const url = `${origin}/${entity}`
+
+      const load = async () => {
+        if ($http) {
+          const {body} = await $http.post(
+            url,
+            data,
+            { headers }
+          )
+          return body
+        } else {
+          const response = await fetch(
+            url,
+            {
+              method: 'POST',
+              body: JSON.stringify(data),
+              headers
+            }
+          )
+
+          const json = await response.json()
+
+          return json
+        }
+      }
+
+      const {suggestions} = await load()
+
+      return suggestions
+    })
+  }
+})
